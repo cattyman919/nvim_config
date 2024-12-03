e5b7416765078da11fa5f304f74a0a65b9579961
Cattyman <88530324+cattyman919@users.noreply.github.com>
2024-12-03 23:40:44 +0700

    added battery plugin

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,10 +1,19 @@
 return {
-  "nvim-lualine/lualine.nvim",
-  config = function()
-    require('lualine').setup({
-      options = {
-        theme = 'dracula'
-      }
-    })
-  end
+	"nvim-lualine/lualine.nvim",
+	config = function()
+		require("battery").setup({})
+		local nvimbattery = {
+			function()
+				return require("battery").get_status_line()
+			end,
+		}
+		require("lualine").setup({
+			options = {
+				theme = "ayu_mirage",
+			},
+			sections = {
+        lualine_x = { nvimbattery, 'filetype'},
+			},
+		})
+	end,
 }
